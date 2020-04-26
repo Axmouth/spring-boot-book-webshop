1bd576b964441170ca744ef55baa307903232688
Axmouth <aggronfg@yahoo.com>
2020-04-26 04:30:59 +0300

    first commit

---
+++ src/main/java/com/axmouth/bookwebshop/BookWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.axmouth.bookwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axmouth/bookwebshop/BookWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axmouth.bookwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
